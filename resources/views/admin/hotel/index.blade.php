ead2241d78171c6a4e98d01f728c462b40e054fd
RobertML41 <m320d4ky1983@bangkit.academy>
2024-07-03 18:21:26 +0800

    tambah policy untuk type hote dan type product

--- resources/views/admin/hotel/index.blade.php
+++ resources/views/admin/hotel/index.blade.php
@@ -5,27 +5,39 @@
   <a href="{{ route('admin.hotel.createAdmin') }}" class="btn btn-primary">Create Hotel</a>
 </div>
 <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">name</th>
-        <th scope="col">telephone</th>
-        <th scope="col">owner</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($hotels as $hotel)
-      <tr>
-        <th scope="row">{{ $loop->iteration }}</th>
-        <td>{{ $hotel->name }}</td>
-        <td>{{ $hotel->telephone }}</td>
-        <td>{{ $hotel->user->name }}</td>
-        <td><a href="{{ route('admin.hotel.showAdmin', $hotel) }}" class="btn btn-primary">Detail</a> || <a href="{{ route('admin.hotel.editAdmin', $hotel) }}" class="btn btn-secondary">Edit</a></td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">name</th>
+      <th scope="col">telephone</th>
+      <th scope="col">owner</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($hotels as $hotel)
+    <tr>
+      <th scope="row">{{ $loop->iteration }}</th>
+      <td>{{ $hotel->name }}</td>
+      <td>{{ $hotel->telephone }}</td>
+      <td>{{ $hotel->user->name }}</td>
+      <td>
+        <a href="{{ route('admin.hotel.showAdmin', $hotel) }}" class="btn btn-primary">Detail</a>
+        ||
+        <a href="{{ route('admin.hotel.editAdmin', $hotel) }}" class="btn btn-secondary">Edit</a>
+      </td>
+      ||
+      <td>
+        <form method="POST">
+          @csrf
+          @method('DELETE')
+          <input type="submit" value="delete" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete Transaction')">
+        </form>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
 
 @endsection
 
